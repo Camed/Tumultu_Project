c5b2eaa94ecd8afe6177bb726754776f2bca270c
Gabriel <camed.tf@gmail.com>
2023-12-03 20:58:04 +0100

    TUM-06 - Fixing missing project references

--- Backend/Tumultu/Application/Common/Interfaces/IIdentityService.cs
+++ Backend/Tumultu/Application/Common/Interfaces/IIdentityService.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Tumultu.Application.Common.Models;
+using Tumultu.Domain.Constants;
+using Tumultu.Domain.Entities;
 
-namespace Application.Common.Interfaces
+namespace Tumultu.Application.Common.Interfaces;
+
+public interface IIdentityService
 {
-    internal interface IIdentityService
-    {
-    }
+    Task<User?> GetUserById(Guid id);
+    Task<bool> IsInRoleAsync(Guid? id, Roles role);
+    Task<bool> AuthorizeAsync(Guid? Id, Roles role);
+    Task<(Result result, Guid userId)> CreateUserAsync(string userName, string password);
+    Task<Result> DeleteUserAsync(Guid userId);
+
 }
