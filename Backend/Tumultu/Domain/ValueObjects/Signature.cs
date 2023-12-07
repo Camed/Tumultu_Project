1b06f267a02aaee6112d2ad65c0a5eafb2517d3f
Gabriel <camed.tf@gmail.com>
2023-12-07 15:13:55 +0100

    Small fix - changing signature, now we look at it as  value object, not an entity

---
+++ Backend/Tumultu/Domain/ValueObjects/Signature.cs
@@ -0,0 +1,28 @@
+﻿using Tumultu.Domain.Enums;
+using Tumultu.Domain.Common;
+
+namespace Tumutlu.Domain.ValueObjects;
+
+public class Signature : ValueObject
+{
+    public Signature() { }
+    public Signature(byte[]? bytes, Endianess endianess, string? name, string description)
+    {
+        this.Description = description;
+        this.SignatureEndianess = endianess;
+        this.Name = name;
+        this.SignatureBytes = bytes;
+    }
+    public byte[]? SignatureBytes { get; set; }
+    public Endianess SignatureEndianess { get; set; } = Endianess.LittleEndian;
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return SignatureBytes!;
+        yield return SignatureEndianess;
+        yield return Name!;
+        yield return Description!;
+    }
+}
