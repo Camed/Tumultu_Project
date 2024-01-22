6901e867832402b5fe562e6b80cc76dcb334dc01
Paweł Augustyniak <paugustyniak1213@gmail.com>
2024-01-22 21:37:38 +0100

    TUM-29 - add owning of Notes by User to entity configuration

--- Backend/Tumultu/Infrastructure/Database/EFCore/Configuration/UserEntityTypeConfiguration.cs
+++ Backend/Tumultu/Infrastructure/Database/EFCore/Configuration/UserEntityTypeConfiguration.cs
@@ -8,7 +8,8 @@ internal class UserEntityTypeConfiguration : IEntityTypeConfiguration<User>
 {
     public void Configure(EntityTypeBuilder<User> builder)
     {
-      
+        builder.OwnsMany(entity => entity.Notes);
+        
         //TODO add property constraints and indexes (ex. like max length for string values)
     }
 }
